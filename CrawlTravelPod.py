e9c6ad1e55746b7e6d7e22a2e19032a6226c1655
Trent Niemeyer <trent@peaksware.com>
2015-06-10 21:39:09 -0600

    parse author and blog

--- CrawlTravelPod.py
+++ CrawlTravelPod.py
@@ -1,8 +1,10 @@
 import TravelPodParser
+import json
 
 if __name__ == '__main__':
     parser = TravelPodParser.BlogParser("http://www.travelpod.com/travel-blog-entries/bridie.sheehan/1/1433692810/tpod.html", False)
-    parser.parseMainContent()
-    parser.parseLocation()
-    parser.parseAuthor()
-    parser.saveBlogAndAuthor()
+    parser.parseall()
+    #print json.dumps(parser.data)
+    #print json.dumps(parser.authorparser.data)
+    print parser.authorparser.save()
+    print parser.save()
--- TravelPodParser.py
+++ TravelPodParser.py
@@ -9,20 +9,30 @@ except ImportError:
     # Fall back to Python 2's urllib2
     from urllib2 import urlopen
 
-class BlogParser (object):
-
-    #url = "http://www.travelpod.com/travel-blog-entries/bridie.sheehan/1/1433692810/tpod.html"
+class Parser (object):
 
     def __init__(self, url, forceReindex):
         self.url = url
         self.es = elasticsearch.Elasticsearch()
-        #TODO: See if URL already exists and 'forceReIndex' is true
+        #TODO: See if URL already exists and 'for`ceReIndex' is true
         self.response = urlopen(url)
         self.html = self.response.read().decode('utf-8')
         self.soup = BeautifulSoup.BeautifulSoup(self.html)
         self.data = {}
+        self.data['url'] = url
 
-    def parseMainContent (self):
+class BlogParser (Parser):
+
+    def __init__(self, url, forceReindex):
+        Parser.__init__(self, url, forceReindex)
+
+    def parseall (self):
+        self.parsemaincontent()
+        self.parselocation()
+        self.parseauthor()
+        self.parsetrip()
+
+    def parsemaincontent (self):
         postBody = self.soup.find(id="post")
         #TODO-LOW: If PostBody Not Found, Throw Error.  Write failing test for this, it means the site changed
         postBodyText = ''
@@ -33,7 +43,7 @@ class BlogParser (object):
         self.data['text']= postBodyText,
         self.data['length'] = len(postBodyText)
 
-    def parseLocation(self):
+    def parselocation(self):
         locationStack = []
         date = ''
         titleContents = self.soup.find("p", attrs={'class' : 'meta'}).contents
@@ -51,12 +61,43 @@ class BlogParser (object):
         self.data ['country'] = locationStack[2]
         self.data['postDate'] = date.strftime('%Y-%m-%dT%H:%M:%S%z')
 
-    def saveBlogAndAuthor (self):
+    def save (self):
         result = self.es.index(index='blogs', doc_type='blog', body=json.dumps (self.data))
-        print result
-        #TODO-NOW Save Author
+        return result
 
-    def parseAuthor (self):
+    def parseauthor (self):
         authorHref = self.soup.find("a", attrs={'class' : 'avatar'})['href']
         authorLink = "http://www.travelpod.com" + authorHref
+        self.authorparser = AuthorParser (authorLink, False)
+        self.authorparser.parselogsummary()
+
+    def parsetrip (self):
+        self.data['trip'] = 'http://www.travelpod.com' + self.soup.find("a", attrs={'title' : 'See more entries in this travel blog'})['href']
+
+class AuthorParser (Parser):
+
+    def __init__(self, url, forceReindex):
+        Parser.__init__(self, url, forceReindex)
+
+    def parselogsummary (self):
+        self.data['username'] = self.soup.find("meta", {"property":"og:title"})['content']
+        summary = self.soup.find("div", attrs={'class' : 'bubble'}).contents
+        for t in summary:
+            if type(t) is BeautifulSoup.Tag:
+                if t.name == 'span':
+                    self.data['blogCount'] = str(t.string).split(' ')[0]
+        self.data['photo'] = self.soup.find(id="profile_pic")['src']
+
+    def parsetrips (self):
+        print ("TODO")
+
+    def save (self):
+        result = self.es.index(index='authors', doc_type='author', body=json.dumps (self.data))
+        return result
+
+
+
+
+
+
 
